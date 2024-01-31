2d5767a3575549cb37b8d65344102b9d6e36b2fc
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-01-31 19:47:51 +0100

    feat(overlay): add overlay content positioning on window resize, refactor service

--- src/app/core/services/overlay/overlay.service.ts
+++ src/app/core/services/overlay/overlay.service.ts
@@ -1,5 +1,5 @@
 import {
-    ApplicationRef, ComponentRef, createComponent, Injectable, Renderer2, RendererFactory2, Type,
+    ApplicationRef, ComponentRef, createComponent, Injectable, NgZone, Renderer2, RendererFactory2, Type,
 } from '@angular/core';
 
 import { ComponentInputs } from '../../models/component-inputs';
@@ -9,15 +9,18 @@ import { OverlayOptions } from '../../models/overlay-options';
     providedIn: 'root',
 })
 export class OverlayService<T> {
-    private overlay: Element;
+    private overlay: HTMLDivElement;
+    private overlayContent: HTMLDivElement | null = null;
     private backdrop: HTMLDivElement | null = null;
-    private backdropListener: (() => void) | null = null;
+    private disposeBackdropListener: (() => void) | null = null;
+    private disposeResizeListener: (() => void) | null = null;
     private componentRef: ComponentRef<T> | null = null;
     private renderer: Renderer2;
 
     constructor(
         private applicationRef: ApplicationRef,
         private rendererFactory: RendererFactory2,
+        private ngZone: NgZone,
     ) {
         this.renderer = this.rendererFactory.createRenderer(null, null);
         this.overlay = document.getElementsByClassName('overlay-container')[0] as HTMLDivElement;
@@ -26,51 +29,15 @@ export class OverlayService<T> {
     show(component: Type<T>, options?: OverlayOptions): void {
         this.showOverlayContainer();
         this.createBackdrop(options?.background, options?.closeOnBackdropClick);
-
-        const overlayContent = this.createOverlayContent(options);
-        this.componentRef = createComponent<T>(component, {
-            environmentInjector: this.applicationRef.injector,
-            hostElement: overlayContent,
-        });
-
-        this.setInputs(this.componentRef, options?.componentInputs);
-        this.applicationRef.attachView(this.componentRef.hostView);
-        this.componentRef.changeDetectorRef.detectChanges();
+        this.createOverlayContent(options);
+        this.createComponent(component, options);
     }
 
     close(): void {
+        this.removeComponent();
+        this.cleanOverlayContent();
+        this.cleanBackdrop();
         this.hideOverlayContainer();
-        this.removeBackdrop();
-        this.applicationRef.detachView(this.componentRef!.hostView);
-        this.componentRef = null;
-    }
-
-    private createOverlayContent(options: OverlayOptions | undefined): HTMLDivElement {
-        const overlayContent = document.createElement('div');
-        overlayContent.classList.add('overlay-content');
-
-        if (options?.directPosition) {
-            overlayContent.style.top = `${options.directPosition?.top}px`;
-            overlayContent.style.bottom = `${options.directPosition?.bottom}px`;
-            overlayContent.style.left = `${options.directPosition?.left}px`;
-            overlayContent.style.right = `${options.directPosition?.right}px`;
-            overlayContent.style.width = `${options.directPosition?.width}px`;
-            overlayContent.style.height = `${options.directPosition?.height}px`;
-        } else if (options?.relativePosition) {
-            const top = Math.round(options.relativePosition.relativeElement?.getBoundingClientRect().top || 0);
-            const left = Math.round(options.relativePosition.relativeElement?.getBoundingClientRect().left || 0);
-            const offsetY = options?.relativePosition.offsetY || 0;
-            const offsetX = options?.relativePosition.offsetX || 0;
-            overlayContent.style.top = `${top + offsetY}px`;
-            overlayContent.style.left = `${left + offsetX}px`;
-            overlayContent.style.width = `${options.relativePosition?.width}px`;
-            overlayContent.style.height = `${options.relativePosition?.height}px`;
-        } else {
-            overlayContent.classList.add('overlay-content-center');
-        }
-
-        this.overlay.appendChild(overlayContent);
-        return overlayContent;
     }
 
     private showOverlayContainer(): void {
@@ -81,10 +48,25 @@ export class OverlayService<T> {
         this.overlay.classList.remove('overlay-container-show');
     }
 
-    private setInputs(componentRef: ComponentRef<T>, inputs: ComponentInputs[] | undefined): void {
-        inputs?.forEach((i) => {
-            componentRef.setInput(i.name, i.value);
+    private createComponent(component: Type<T>, options?: OverlayOptions): void {
+        this.componentRef = createComponent<T>(component, {
+            environmentInjector: this.applicationRef.injector,
+            hostElement: this.overlayContent!,
         });
+        this.setComponentInputs(options?.componentInputs);
+        this.applicationRef.attachView(this.componentRef.hostView);
+        this.componentRef.changeDetectorRef.detectChanges();
+    }
+
+    private setComponentInputs(inputs: ComponentInputs[] | undefined): void {
+        inputs?.forEach((i) => {
+            this.componentRef!.setInput(i.name, i.value);
+        });
+    }
+
+    private removeComponent(): void {
+        this.applicationRef.detachView(this.componentRef!.hostView);
+        this.componentRef = null;
     }
 
     createBackdrop(background = true, closeOnBackdropClick = true): void {
@@ -96,7 +78,7 @@ export class OverlayService<T> {
         }
 
         if (closeOnBackdropClick) {
-            this.backdropListener = this.renderer.listen(this.backdrop, 'click', () => {
+            this.disposeBackdropListener = this.renderer.listen(this.backdrop, 'click', () => {
                 this.close();
             });
         }
@@ -104,10 +86,63 @@ export class OverlayService<T> {
         this.overlay.appendChild(this.backdrop);
     }
 
-    removeBackdrop(): void {
-        if (this.backdropListener) {
-            this.backdropListener();
+    private cleanBackdrop(): void {
+        if (this.disposeBackdropListener) {
+            this.disposeBackdropListener();
         }
+        this.disposeBackdropListener = null;
         this.backdrop?.remove();
+    }
+
+    private createOverlayContent(options: OverlayOptions | undefined): void {
+        this.overlayContent = document.createElement('div');
+        this.overlayContent.classList.add('overlay-content');
+
+        if (options?.directPosition) {
+            this.setOverlayDirectPosition(options);
+        } else if (options?.relativePosition) {
+            this.setOverlayRelativePosition(options);
+            this.ngZone.runOutsideAngular(() => {
+                this.disposeResizeListener = this.renderer.listen(window, 'resize', () => {
+                    this.setOverlayRelativePosition(options);
+                });
+            });
+        } else {
+            this.overlayContent.classList.add('overlay-content-center');
+        }
+
+        this.overlay.appendChild(this.overlayContent);
+    }
+
+    private setOverlayDirectPosition(options: OverlayOptions): void {
+        if (this.overlayContent) {
+            this.overlayContent.style.top = `${options.directPosition?.top}px`;
+            this.overlayContent.style.bottom = `${options.directPosition?.bottom}px`;
+            this.overlayContent.style.left = `${options.directPosition?.left}px`;
+            this.overlayContent.style.right = `${options.directPosition?.right}px`;
+            this.overlayContent.style.width = `${options.directPosition?.width}px`;
+            this.overlayContent.style.height = `${options.directPosition?.height}px`;
+        }
+    }
+
+    private setOverlayRelativePosition(options: OverlayOptions): void {
+        if (this.overlayContent) {
+            const top = Math.round(options.relativePosition?.relativeElement?.getBoundingClientRect().top || 0);
+            const left = Math.round(options.relativePosition?.relativeElement?.getBoundingClientRect().left || 0);
+            const offsetY = options?.relativePosition?.offsetY || 0;
+            const offsetX = options?.relativePosition?.offsetX || 0;
+            this.overlayContent.style.top = `${top + offsetY}px`;
+            this.overlayContent.style.left = `${left + offsetX}px`;
+            this.overlayContent.style.width = `${options.relativePosition?.width}px`;
+            this.overlayContent.style.height = `${options.relativePosition?.height}px`;
+        }
+    }
+
+    private cleanOverlayContent(): void {
+        this.overlayContent = null;
+        if (this.disposeResizeListener) {
+            this.disposeResizeListener();
+        }
+        this.disposeResizeListener = null;
     }
 }
