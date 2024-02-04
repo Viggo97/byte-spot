d96a48a8023311ef8973bee42dd9c93246c5aa03
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-02-04 19:06:03 +0100

    feat(dropdown-container): add component

---
+++ src/app/shared/components/dropdown-container/dropdown-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'bsa-dropdown-container',
+    standalone: true,
+    imports: [],
+    templateUrl: './dropdown-container.component.html',
+    styleUrl: './dropdown-container.component.scss',
+})
+export class DropdownContainerComponent {
+
+}
