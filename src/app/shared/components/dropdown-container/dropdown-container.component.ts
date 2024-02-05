4a879004559296cd2613d1a24c8616a5eda7122b
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-02-05 18:51:54 +0100

    feat(dropdown-container): add base structure and implement comunication

--- src/app/shared/components/dropdown-container/dropdown-container.component.ts
+++ src/app/shared/components/dropdown-container/dropdown-container.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { NgForOf } from '@angular/common';
+import {
+    ChangeDetectionStrategy, Component, EventEmitter, Input, Output,
+} from '@angular/core';
 
+type DropdownOption = { key: string, value: string };
 @Component({
     selector: 'bsa-dropdown-container',
     standalone: true,
-    imports: [],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [
+        NgForOf,
+    ],
     templateUrl: './dropdown-container.component.html',
     styleUrl: './dropdown-container.component.scss',
 })
 export class DropdownContainerComponent {
+    @Input({ required: true })
+    set options(options: Map<string, string>) {
+        this.items = Array.from(options, ([key, value]) => ({ key, value }));
+    }
 
+    items: DropdownOption[] = [];
+
+    @Output() selectItem = new EventEmitter<DropdownOption>();
+
+    onSelectItem(item: DropdownOption): void {
+        this.selectItem.emit(item);
+    }
 }
