8a5531ee67647fbdde0c1bdb4d454115f123393f
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-02-06 18:36:43 +0100

    feat(dropdown-container): add keyboard accessibility

--- src/app/shared/ui/dropdown-container/dropdown-container.component.ts
+++ src/app/shared/ui/dropdown-container/dropdown-container.component.ts
@@ -1,6 +1,6 @@
 import { NgForOf } from '@angular/common';
 import {
-    ChangeDetectionStrategy, Component, EventEmitter, Input, Output,
+    ChangeDetectionStrategy, Component, ElementRef, EventEmitter, HostListener, Input, Output, QueryList, ViewChildren,
 } from '@angular/core';
 
 import { DropdownOption } from './model/dropdown-option';
@@ -16,6 +16,7 @@ import { DropdownOption } from './model/dropdown-option';
     styleUrl: './dropdown-container.component.scss',
 })
 export class DropdownContainerComponent {
+    @Input() tabIndex: number = 0;
     @Input({ required: true })
     set options(options: Map<string, string>) {
         this.items = Array.from(options, ([key, value]) => ({ key, value }));
@@ -25,7 +26,99 @@ export class DropdownContainerComponent {
 
     @Output() selectItem = new EventEmitter<DropdownOption>();
 
-    onSelectItem(item: DropdownOption): void {
+    @ViewChildren('itemRef') itemsRef: QueryList<ElementRef> = new QueryList<ElementRef>();
+
+    index = 0;
+
+    onSelectItem(event: MouseEvent, item: DropdownOption): void {
+        event.preventDefault();
+        event.stopPropagation();
+        this.index = this.items.indexOf(item);
         this.selectItem.emit(item);
+    }
+
+    @HostListener('keydown', ['$event'])
+    onKeyboardNavigation(event: KeyboardEvent): void {
+        event.preventDefault();
+        event.stopPropagation();
+
+        const { code } = event;
+
+        switch (code) {
+            case 'ArrowDown':
+                this.moveIndexAhead();
+                break;
+            case 'ArrowUp':
+                this.moveIndexBack();
+                break;
+            case 'Tab':
+                this.onTab(event);
+                break;
+            default:
+                break;
+        }
+    }
+
+    @HostListener('keyup', ['$event'])
+    onKeyboardAction(event: KeyboardEvent): void {
+        event.preventDefault();
+        event.stopPropagation();
+
+        const { code } = event;
+
+        switch (code) {
+            case 'Enter':
+                this.handleSelectItemByKeyboard();
+                break;
+            case 'Space':
+                this.handleSelectItemByKeyboard();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private handleSelectItemByKeyboard(): void {
+        const selectedItem = this.items[this.index];
+        this.selectItem.emit(selectedItem);
+    }
+
+    private moveIndexAhead(): void {
+        if (!this.isLastIndex) {
+            this.index += 1;
+            this.focusItem();
+        }
+    }
+
+    private moveIndexBack(): void {
+        if (!this.isFirstIndex) {
+            this.index -= 1;
+            this.focusItem();
+        }
+    }
+
+    private onTab(event: KeyboardEvent): void {
+        if (event.shiftKey) {
+            this.moveIndexBack();
+        } else {
+            this.moveIndexAhead();
+        }
+    }
+
+    private get isFirstIndex(): boolean {
+        return this.index === 0;
+    }
+
+    private get isLastIndex(): boolean {
+        return this.index === this.items.length - 1;
+    }
+
+    private focusItem(): void {
+        this.itemsRef.get(this.index)?.nativeElement.focus();
+        this.itemsRef.get(this.index)?.nativeElement.scrollIntoView({
+            behavior: 'auto',
+            block: 'center',
+            inline: 'center',
+        });
     }
 }
