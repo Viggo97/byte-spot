52da42cb916bf5c537219d77908d3a8f5aaeb813
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-02-09 18:25:23 +0100

    test(dropdown-container): add tests

--- src/app/shared/ui/dropdown-container/dropdown-container.component.spec.ts
+++ src/app/shared/ui/dropdown-container/dropdown-container.component.spec.ts
@@ -1,10 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { DropdownContainerComponent } from './dropdown-container.component';
+
+const options = new Map()
+    .set('opt1', 'Option 1')
+    .set('opt2', 'Option 2')
+    .set('opt3', 'Option 3')
+    .set('opt4', 'Option 4')
+    .set('opt5', 'Option 5');
 
 describe('DropdownContainerComponent', () => {
     let component: DropdownContainerComponent;
     let fixture: ComponentFixture<DropdownContainerComponent>;
+
+    const dispatchEvent = (event: KeyboardEvent, times: number = 7) => {
+        for (let i = 0; i < times; i += 1) {
+            fixture.nativeElement.dispatchEvent(event);
+        }
+    };
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
@@ -14,10 +28,55 @@ describe('DropdownContainerComponent', () => {
 
         fixture = TestBed.createComponent(DropdownContainerComponent);
         component = fixture.componentInstance;
+        component.options = options;
+        component.tabIndex = 1000;
         fixture.detectChanges();
     });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
+    afterEach(() => {
+        fixture.destroy();
+    });
+
+    it('should render items in container', () => {
+        const container = fixture.debugElement.query(By.css('.dropdown-container'));
+        const items = fixture.debugElement.queryAll(By.css('.dropdown-item'));
+        expect(container).toBeTruthy();
+        expect(items).toHaveSize(5);
+        expect(items[0].nativeElement.getAttribute('tabindex')).toEqual('1000');
+    });
+
+    it('should navigate through items by arrows and tab', () => {
+        const arrowDownEvent = new KeyboardEvent('keydown', { code: 'ArrowDown' });
+        const arrowUpEvent = new KeyboardEvent('keydown', { code: 'ArrowUp' });
+        const tabEvent = new KeyboardEvent('keydown', { code: 'Tab' });
+        const tabShiftEvent = new KeyboardEvent('keydown', { code: 'Tab', shiftKey: true });
+        const firstItem = fixture.debugElement.queryAll(By.css('.dropdown-item'))[0].nativeElement;
+        const lastItem = fixture.debugElement.queryAll(By.css('.dropdown-item'))[4].nativeElement;
+        const firstItemFocusSpy = spyOn(firstItem, 'focus').and.callThrough();
+        const lastItemFocusSpy = spyOn(lastItem, 'focus').and.callThrough();
+
+        dispatchEvent(arrowDownEvent);
+
+        expect(firstItemFocusSpy).toHaveBeenCalledTimes(1);
+        expect(lastItemFocusSpy).toHaveBeenCalledTimes(1);
+        expect(lastItem).toEqual(document.activeElement);
+
+        dispatchEvent(arrowUpEvent);
+
+        expect(firstItemFocusSpy).toHaveBeenCalledTimes(2);
+        expect(lastItemFocusSpy).toHaveBeenCalledTimes(1);
+        expect(firstItem).toEqual(document.activeElement);
+
+        dispatchEvent(tabEvent);
+
+        expect(firstItemFocusSpy).toHaveBeenCalledTimes(2);
+        expect(lastItemFocusSpy).toHaveBeenCalledTimes(2);
+        expect(lastItem).toEqual(document.activeElement);
+
+        dispatchEvent(tabShiftEvent);
+
+        expect(firstItemFocusSpy).toHaveBeenCalledTimes(3);
+        expect(lastItemFocusSpy).toHaveBeenCalledTimes(2);
+        expect(firstItem).toEqual(document.activeElement);
     });
 });
