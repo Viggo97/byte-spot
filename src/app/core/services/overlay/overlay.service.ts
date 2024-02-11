bbaf50156aa0c17d94fc0a49a6e5a73fb9f1b7e1
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-02-11 18:06:17 +0100

    fix(overlay): destroy component on close

--- src/app/core/services/overlay/overlay.service.ts
+++ src/app/core/services/overlay/overlay.service.ts
@@ -76,6 +76,7 @@ export class OverlayService<T> {
     }
 
     private removeComponent(): void {
+        this.componentRef?.destroy();
         this.applicationRef.detachView(this.componentRef!.hostView);
         this.componentRef = null;
     }
