d554a3f52ae0ea48a972140e938b459ed7e1de60
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-02-14 19:35:20 +0100

    feat(language-service): handle language change

--- src/app/core/services/language/language.service.ts
+++ src/app/core/services/language/language.service.ts
@@ -1,24 +1,45 @@
 import { Injectable } from '@angular/core';
 
 import { DropdownContainerComponent } from '../../../shared/ui/dropdown-container/dropdown-container.component';
+import { DropdownOption } from '../../../shared/ui/dropdown-container/model/dropdown-option';
+import { EdgeX, EdgeY } from '../../models/relative-position-edge';
 import { OverlayService } from '../overlay/overlay.service';
 
 @Injectable({
     providedIn: 'root',
 })
 export class LanguageService {
+    language: string = 'EN';
+
     constructor(private overlayService: OverlayService<DropdownContainerComponent>) { }
 
     openLanguageSelection(relativeElement: HTMLButtonElement): void {
-        const languageOptions = new Map<string, string>().set('en', 'English').set('pl', 'Polish');
+        const languageOptions = new Map<string, string>()
+            .set('EN', 'English')
+            .set('PL', 'Polish');
         this.overlayService.show(DropdownContainerComponent, {
             componentInputs: [
                 { name: 'options', value: languageOptions },
             ],
-            background: false,
+            backdrop: {
+                background: false,
+            },
             relativePosition: {
                 relativeElement,
+                edgePositionX: {
+                    relativeEdge: EdgeX.LEFT,
+                    contentEdge: EdgeX.RIGHT,
+                },
+                edgePositionY: {
+                    relativeEdge: EdgeY.BOTTOM,
+                    contentEdge: EdgeY.TOP,
+                },
             },
+        });
+
+        this.overlayService.outputChange$.subscribe((output) => {
+            this.language = (output.value as DropdownOption).key;
+            this.overlayService.close();
         });
     }
 }
