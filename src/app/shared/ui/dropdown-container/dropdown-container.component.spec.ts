4421b569a85c2ad68c8bc8a68853565a78059f69
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-02-16 18:54:28 +0100

    test(dropdown-container): adjust tests to new logic

--- src/app/shared/ui/dropdown-container/dropdown-container.component.spec.ts
+++ src/app/shared/ui/dropdown-container/dropdown-container.component.spec.ts
@@ -14,9 +14,9 @@ describe('DropdownContainerComponent', () => {
     let component: DropdownContainerComponent;
     let fixture: ComponentFixture<DropdownContainerComponent>;
 
-    const dispatchEvent = (event: KeyboardEvent | FocusEvent, times: number = 7) => {
+    const dispatchEvent = (element: Window | Element, event: KeyboardEvent, times: number = 7) => {
         for (let i = 0; i < times; i += 1) {
-            fixture.nativeElement.dispatchEvent(event);
+            element.dispatchEvent(event);
         }
     };
 
@@ -63,25 +63,25 @@ describe('DropdownContainerComponent', () => {
         const firstItemFocusSpy = spyOn(firstItem, 'focus').and.callThrough();
         const lastItemFocusSpy = spyOn(lastItem, 'focus').and.callThrough();
 
-        dispatchEvent(arrowDownEvent);
+        dispatchEvent(window, arrowDownEvent);
 
         expect(firstItemFocusSpy).toHaveBeenCalledTimes(1);
         expect(lastItemFocusSpy).toHaveBeenCalledTimes(1);
         expect(lastItem).toEqual(document.activeElement);
 
-        dispatchEvent(arrowUpEvent);
+        dispatchEvent(window, arrowUpEvent);
 
         expect(firstItemFocusSpy).toHaveBeenCalledTimes(2);
         expect(lastItemFocusSpy).toHaveBeenCalledTimes(1);
         expect(firstItem).toEqual(document.activeElement);
 
-        dispatchEvent(tabEvent);
+        dispatchEvent(window, tabEvent);
 
         expect(firstItemFocusSpy).toHaveBeenCalledTimes(2);
         expect(lastItemFocusSpy).toHaveBeenCalledTimes(2);
         expect(lastItem).toEqual(document.activeElement);
 
-        dispatchEvent(tabShiftEvent);
+        dispatchEvent(window, tabShiftEvent);
 
         expect(firstItemFocusSpy).toHaveBeenCalledTimes(3);
         expect(lastItemFocusSpy).toHaveBeenCalledTimes(2);
@@ -91,27 +91,27 @@ describe('DropdownContainerComponent', () => {
     it('should select item by click', () => {
         const items = fixture.debugElement.queryAll(By.css('.dropdown-item'));
         const onSelectItemSpy = spyOn(component, 'onSelectItem').and.callThrough();
-        const selectItemSpy = spyOn(component.selectItem, 'emit').and.callThrough();
+        const selectItemSpy = spyOn(component.selectOption, 'emit').and.callThrough();
 
         items[0].nativeElement.click();
 
-        expect(onSelectItemSpy).toHaveBeenCalled();
-        expect(selectItemSpy).toHaveBeenCalled();
+        expect(onSelectItemSpy).toHaveBeenCalledTimes(1);
+        expect(selectItemSpy).toHaveBeenCalledTimes(1);
     });
 
     it('should select item by keyboard', () => {
         const arrowDownEvent = new KeyboardEvent('keydown', { code: 'ArrowDown' });
         const enterEvent = new KeyboardEvent('keyup', { code: 'Enter' });
         const spaceEvent = new KeyboardEvent('keyup', { code: 'Space' });
-        const selectItemSpy = spyOn(component.selectItem, 'emit').and.callThrough();
+        const selectItemSpy = spyOn(component.selectOption, 'emit').and.callThrough();
 
-        dispatchEvent(arrowDownEvent, 1);
-        dispatchEvent(enterEvent, 1);
+        dispatchEvent(window, arrowDownEvent, 1);
+        dispatchEvent(fixture.nativeElement, enterEvent, 1);
 
         expect(selectItemSpy).toHaveBeenCalledTimes(1);
 
-        dispatchEvent(arrowDownEvent, 1);
-        dispatchEvent(spaceEvent, 1);
+        dispatchEvent(window, arrowDownEvent, 1);
+        dispatchEvent(fixture.nativeElement, spaceEvent, 1);
 
         expect(selectItemSpy).toHaveBeenCalledTimes(2);
     });
@@ -119,24 +119,36 @@ describe('DropdownContainerComponent', () => {
     it('should NOT trigger any action by other keys', () => {
         const keydownEvent = new KeyboardEvent('keydown', { code: 'ArrowLeft' });
         const keyupEvent = new KeyboardEvent('keyup', { code: 'Delete' });
-        const selectItemSpy = spyOn(component.selectItem, 'emit').and.callThrough();
+        const selectItemSpy = spyOn(component.selectOption, 'emit').and.callThrough();
 
-        dispatchEvent(keydownEvent, 1);
-        dispatchEvent(keyupEvent, 1);
+        dispatchEvent(window, keydownEvent, 1);
+        dispatchEvent(fixture.nativeElement, keyupEvent, 1);
 
         expect(selectItemSpy).not.toHaveBeenCalled();
     });
 
-    it('should set and handle focus in and out', () => {
-        const onFocusInSpy = spyOn(component, 'onFocusIn').and.callThrough();
-        const onFocusOutSpy = spyOn(component, 'onFocusOut').and.callThrough();
-        const focusinEvent = new FocusEvent('focusin');
-        const focusoutEvent = new FocusEvent('focusout');
+    it('should set reset tabindex', () => {
+        const keydownEvent = new KeyboardEvent('keydown', { code: 'ArrowDown' });
+        const clickEvent = new MouseEvent('click');
+        const firstItem = fixture.debugElement.queryAll(By.css('.dropdown-item'))[0].nativeElement;
+        const thirdItem = fixture.debugElement.queryAll(By.css('.dropdown-item'))[2].nativeElement;
 
-        dispatchEvent(focusinEvent);
-        dispatchEvent(focusoutEvent);
+        dispatchEvent(window, keydownEvent, 3);
 
-        expect(onFocusInSpy).toHaveBeenCalled();
-        expect(onFocusOutSpy).toHaveBeenCalled();
+        expect(document.activeElement).toEqual(thirdItem);
+
+        fixture.componentInstance.onClick(clickEvent);
+        dispatchEvent(window, keydownEvent, 1);
+
+        expect(document.activeElement).toEqual(firstItem);
+    });
+
+    it('should navigate through items starting from end', () => {
+        const keyupEvent = new KeyboardEvent('keydown', { code: 'ArrowUp' });
+        const lastItem = fixture.debugElement.queryAll(By.css('.dropdown-item'))[4].nativeElement;
+
+        dispatchEvent(window, keyupEvent, 1);
+
+        expect(document.activeElement).toEqual(lastItem);
     });
 });
