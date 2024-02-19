f7d4043dc02d6edd367562b1d5956c51bafa1ea2
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-02-19 18:30:26 +0100

    refactor(navbar): add language selection

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -2,9 +2,13 @@ import { NgClass } from '@angular/common';
 import { Component, ElementRef, ViewChild } from '@angular/core';
 
 import { DropdownContainerComponent } from '../../../shared/components/dropdown-container/dropdown-container.component';
+import { Language } from '../../enums/language/language.enum';
+import { EdgeX, EdgeY } from '../../enums/overlay/relative-position-edge.enum';
 import { Theme } from '../../enums/theme/theme.enum';
 import { LanguageService } from '../../services/language/language.service';
+import { OverlayService } from '../../services/overlay/overlay.service';
 import { ThemeService } from '../../services/theme/theme.service';
+import { TranslateService } from '../../services/translate/translate.service';
 
 @Component({
     selector: 'bsa-navbar',
@@ -19,8 +23,10 @@ export class NavbarComponent {
     darkTheme: boolean = false;
 
     constructor(
-        private themeService: ThemeService,
         private languageService: LanguageService,
+        private overlayService: OverlayService<DropdownContainerComponent>,
+        private themeService: ThemeService,
+        private translateService: TranslateService,
     ) {
         this.themeService.theme$.subscribe((theme) => {
             this.darkTheme = theme === Theme.DARK;
@@ -32,6 +38,33 @@ export class NavbarComponent {
     }
 
     onChangeLanguage(): void {
-        this.languageService.openLanguageSelection(this.languageButton!.nativeElement);
+        const languageOptions = new Map<string, string>()
+            .set(Language.ENGLISH, this.translateService.translate('global.languageEN'))
+            .set(Language.POLISH, this.translateService.translate('global.languagePL'));
+
+        this.overlayService.show(DropdownContainerComponent, {
+            componentInputs: [
+                { name: 'options', value: languageOptions },
+            ],
+            backdrop: {
+                background: false,
+            },
+            relativePosition: {
+                relativeElement: this.languageButton!.nativeElement,
+                edgePositionX: {
+                    relativeEdge: EdgeX.LEFT,
+                    contentEdge: EdgeX.RIGHT,
+                },
+                edgePositionY: {
+                    relativeEdge: EdgeY.BOTTOM,
+                    contentEdge: EdgeY.TOP,
+                },
+            },
+        });
+
+        this.overlayService.outputChange$.subscribe((output) => {
+            this.languageService.setLanguage(output.value.key as Language);
+            this.overlayService.close();
+        });
     }
 }
