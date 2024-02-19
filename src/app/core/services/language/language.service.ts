f36337eec36977f3fa0de02875902a07d752f034
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-02-19 18:29:32 +0100

    refactor(language-service): remove language selection

--- src/app/core/services/language/language.service.ts
+++ src/app/core/services/language/language.service.ts
@@ -1,47 +1,16 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
-import { DropdownContainerComponent } from '../../../shared/components/dropdown-container/dropdown-container.component';
-import { DropdownOption } from '../../../shared/models/dropdown-container/dropdown-option';
 import { Language } from '../../enums/language/language.enum';
-import { EdgeX, EdgeY } from '../../enums/overlay/relative-position-edge.enum';
-import { OverlayService } from '../overlay/overlay.service';
 
 @Injectable({
     providedIn: 'root',
 })
 export class LanguageService {
-    language = Language.ENGLISH;
+    private language = new BehaviorSubject<Language>(Language.ENGLISH);
+    language$ = this.language.asObservable();
 
-    constructor(private overlayService: OverlayService<DropdownContainerComponent>) { }
-
-    openLanguageSelection(relativeElement: HTMLButtonElement): void {
-        const languageOptions = new Map<string, string>()
-            .set(Language.ENGLISH, 'English')
-            .set(Language.POLISH, 'Polish');
-
-        this.overlayService.show(DropdownContainerComponent, {
-            componentInputs: [
-                { name: 'options', value: languageOptions },
-            ],
-            backdrop: {
-                background: false,
-            },
-            relativePosition: {
-                relativeElement,
-                edgePositionX: {
-                    relativeEdge: EdgeX.LEFT,
-                    contentEdge: EdgeX.RIGHT,
-                },
-                edgePositionY: {
-                    relativeEdge: EdgeY.BOTTOM,
-                    contentEdge: EdgeY.TOP,
-                },
-            },
-        });
-
-        this.overlayService.outputChange$.subscribe((output) => {
-            this.language = ((output.value as DropdownOption).key) as Language;
-            this.overlayService.close();
-        });
+    setLanguage(value: Language): void {
+        this.language.next(value);
     }
 }
