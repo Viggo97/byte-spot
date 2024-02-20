81f7ff86587bc32d97f2d488f450292457c649f1
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-02-20 18:19:41 +0100

    test(language): add tests

--- src/app/core/services/language/language.service.spec.ts
+++ src/app/core/services/language/language.service.spec.ts
@@ -1,16 +1,27 @@
 import { TestBed } from '@angular/core/testing';
 
+import { Language } from '../../enums/language/language.enum';
 import { LanguageService } from './language.service';
 
 describe('LanguageService', () => {
-  let service: LanguageService;
+    let service: LanguageService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(LanguageService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(LanguageService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should have english as default language', () => {
+        service.language$.subscribe((value) => {
+            expect(value).toEqual(Language.ENGLISH);
+        });
+    });
+
+    it('should change language', () => {
+        service.setLanguage(Language.POLISH);
+
+        service.language$.subscribe((value) => {
+            expect(value).toEqual(Language.POLISH);
+        });
+    });
 });
