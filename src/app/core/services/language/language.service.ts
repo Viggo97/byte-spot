e4a0fcbd07428c5c5d0ccb33b3937bb8813b9080
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-02-21 18:17:47 +0100

    feat(language): set default language on app init

--- src/app/core/services/language/language.service.ts
+++ src/app/core/services/language/language.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 import { Language } from '../../enums/language/language.enum';
 
@@ -7,10 +7,46 @@ import { Language } from '../../enums/language/language.enum';
     providedIn: 'root',
 })
 export class LanguageService {
-    private language = new BehaviorSubject<Language>(Language.ENGLISH);
-    language$ = this.language.asObservable();
+    private readonly storageKey = 'language';
+    private language: BehaviorSubject<Language>;
+    language$: Observable<Language>;
+
+    constructor() {
+        const lang = this.getDefaultLanguage();
+        this.language = new BehaviorSubject<Language>(lang);
+        this.language$ = this.language.asObservable();
+    }
+
+    private getDefaultLanguage(): Language {
+        const languageFromStorage = this.getDefaultLanguageFromLocalStorage();
+
+        if (languageFromStorage) {
+            return languageFromStorage;
+        }
+
+        const preferredLanguage = window.navigator.language;
+
+        if (preferredLanguage) {
+            return preferredLanguage.includes('en') ? Language.ENGLISH : Language.POLISH;
+        }
+
+        return Language.ENGLISH;
+    }
+
+    private getDefaultLanguageFromLocalStorage(): Language | null {
+        const availableLanguages = Object.values(Language) as string[];
+        const language = localStorage.getItem('language');
+        if (language && availableLanguages.includes(language)) {
+            return language as Language;
+        }
+        return null;
+    }
 
     setLanguage(value: Language): void {
+        if (value === this.language.value) {
+            return;
+        }
+        localStorage.setItem(this.storageKey, value);
         this.language.next(value);
     }
 }
