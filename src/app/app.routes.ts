47068c32abd804b4ef4dbc6d159474cd0d143a2c
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-02-24 10:45:28 +0100

    feat(route): add offers route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { OffersComponent } from '@app/features/pages/offers/offers.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'offers',
+        component: OffersComponent,
+    },
+    {
+        path: '',
+        redirectTo: '/offers',
+        pathMatch: 'full',
+    },
+];
