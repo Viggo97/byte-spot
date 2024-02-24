bf11aaaeb6218a87c773428c2840e91ff395783e
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-02-24 10:39:12 +0100

    feat(offers): add component

---
+++ src/app/features/pages/offers/offers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'bsa-offers',
+    standalone: true,
+    imports: [],
+    templateUrl: './offers.component.html',
+    styleUrl: './offers.component.scss',
+})
+export class OffersComponent {
+
+}
