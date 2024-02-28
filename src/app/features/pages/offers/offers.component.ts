a18ccda86430a2bab44f0b52117af579daf85242
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-02-28 20:08:35 +0100

    feat(offers): add mock data

--- src/app/features/pages/offers/offers.component.ts
+++ src/app/features/pages/offers/offers.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { OfferComponent } from '@app/features/components/offer/offer.component';
+import { Offer } from '@app/features/model/offer/offer.model';
 
 @Component({
     selector: 'bsa-offers',
     standalone: true,
-    imports: [],
+    imports: [
+        OfferComponent,
+    ],
     templateUrl: './offers.component.html',
     styleUrl: './offers.component.scss',
 })
 export class OffersComponent {
-
+    offers: Offer[] = [
+        {
+            id: 'o1',
+            title: 'Senior Java Developer',
+            companyId: 'c1',
+            companyName: 'Oracle',
+            location: 'San Francisco',
+            minSalary: 17000,
+            maxSalary: 22000,
+            technologyLabels: ['Java', 'Spring', 'Hibernate'],
+        },
+        {
+            id: 'o2',
+            title: 'Mid .Net Developer',
+            companyId: 'c2',
+            companyName: 'Microsoft',
+            location: 'Los Angeles',
+            minSalary: 11000,
+            maxSalary: 16000,
+            technologyLabels: ['C#', '.Net', 'Entity Framework'],
+        },
+        {
+            id: 'o3',
+            title: 'Junior JavaScript Developer',
+            companyId: 'c3',
+            companyName: 'Google',
+            location: 'New York',
+            minSalary: 6000,
+            maxSalary: 9000,
+            technologyLabels: ['JavaScript', 'Angular', 'TypeScript', 'HTML', 'Node.js'],
+        },
+    ];
 }
