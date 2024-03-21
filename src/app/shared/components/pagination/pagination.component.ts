5fbb226a1a110b389940ba1265dfa7f39e40813c
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-03-21 19:55:13 +0100

    refactor(pagination): move number of page to pagination component

--- src/app/shared/components/pagination/pagination.component.ts
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -1,17 +1,21 @@
 import {
     Component, EventEmitter, Input, OnInit, Output,
 } from '@angular/core';
+import { SelectComponent } from '@app/shared/components/select/select.component';
 
 @Component({
     selector: 'bsa-pagination',
     standalone: true,
-    imports: [],
+    imports: [
+        SelectComponent,
+    ],
     templateUrl: './pagination.component.html',
     styleUrl: './pagination.component.scss',
 })
 export class PaginationComponent implements OnInit {
     @Input({ required: true }) total!: number;
-    @Input({ required: true }) perPage!: number;
+    @Input() perPage = 20;
+    @Input() options: number[] = [];
 
     @Input() set currentPage(value: number) {
         this.page = value;
@@ -21,9 +25,23 @@ export class PaginationComponent implements OnInit {
 
     page = 1;
     pages = 1;
+    optionsMap = new Map<string, string>();
 
     ngOnInit(): void {
         this.pages = this.total / this.perPage;
+        this.setOptions();
+    }
+
+    private setOptions(): void {
+        if (this.options.length > 0) {
+            this.options.forEach((o) => this.optionsMap.set(o.toString(), o.toString()));
+        } else {
+            this.optionsMap = new Map<string, string>()
+                .set('10', '10')
+                .set('20', '20')
+                .set('50', '50')
+                .set('100', '100');
+        }
     }
 
     onNextPage(): void {
