1bddb38a8e6cacd5e63a7d0033518201cfdf2938
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-03-23 18:21:51 +0100

    test(navbar): fix test after overlay implementation change

--- src/app/core/components/navbar/navbar.component.spec.ts
+++ src/app/core/components/navbar/navbar.component.spec.ts
@@ -1,11 +1,12 @@
 import { EventEmitter } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NavbarComponent } from '@app/core/components/navbar/navbar.component';
+import { Language } from '@app/core/enums/language/language.enum';
 import { Theme } from '@app/core/enums/theme/theme.enum';
 import { LanguageService } from '@app/core/services/language/language.service';
 import { OverlayService } from '@app/core/services/overlay/overlay.service';
 import { ThemeService } from '@app/core/services/theme/theme.service';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, of } from 'rxjs';
 
 class ThemeServiceStub {
     theme = new BehaviorSubject<Theme>(Theme.LIGHT);
@@ -57,16 +58,17 @@ describe('NavbarComponent', () => {
         const setLanguageSpy = spyOn(languageService, 'setLanguage').and.callThrough();
         const outputStub = new EventEmitter<any>();
         const showOverlaySpy = spyOn(overlayService, 'show')
-            .and.returnValue({ instance: { selectOption: outputStub } });
-        const closeOverlaySpy = spyOn(overlayService, 'close').and.returnValue(null);
+            .and.returnValue([{ instance: { selectOption: outputStub } }, of()]);
+        const closeOverlaySpy = spyOn(overlayService, 'close')
+            .and.returnValue(null);
         const changeLanguageButton = fixture.nativeElement
             .querySelector('#change-language-button') as HTMLButtonElement;
 
         changeLanguageButton.click();
-        outputStub.emit('en-US');
+        outputStub.emit({ key: Language.ENGLISH, value: 'English' });
 
         expect(changeLanguageSpy).toHaveBeenCalled();
-        expect(setLanguageSpy).toHaveBeenCalled();
+        expect(setLanguageSpy).toHaveBeenCalledOnceWith(Language.ENGLISH);
         expect(showOverlaySpy).toHaveBeenCalled();
         expect(closeOverlaySpy).toHaveBeenCalled();
     });
