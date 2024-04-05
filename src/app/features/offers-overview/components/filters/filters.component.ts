b3c997df0282b44537ea2406459f78a11dfafcdb
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-04-05 20:14:44 +0200

    feat(filters): handle showing filter buttons on screen width change

--- src/app/features/offers-overview/components/filters/filters.component.ts
+++ src/app/features/offers-overview/components/filters/filters.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Breakpoints } from '@app/core/enums/breakpoints/breakpoints.enum';
+import { fromEvent, Subject, takeUntil } from 'rxjs';
 
 @Component({
     selector: 'bsa-filters',
@@ -7,8 +9,30 @@ import { Component } from '@angular/core';
     templateUrl: './filters.component.html',
     styleUrl: './filters.component.scss',
 })
-export class FiltersComponent {
+export class FiltersComponent implements OnInit, OnDestroy {
+    resize$ = fromEvent(window, 'resize');
+    filterButtonsVisible = window.innerWidth < Breakpoints.SM;
+
+    private destroy$ = new Subject<void>();
+
+    ngOnInit(): void {
+        this.initFilterIconsHandling();
+    }
+
+    private initFilterIconsHandling(): void {
+        this.resize$
+            .pipe(takeUntil(this.destroy$))
+            .subscribe(() => {
+                this.filterButtonsVisible = window.innerWidth < Breakpoints.SM;
+            });
+    }
+
     onSearchOpen(): void {
 
+    }
+
+    ngOnDestroy(): void {
+        this.destroy$.next();
+        this.destroy$.complete();
     }
 }
