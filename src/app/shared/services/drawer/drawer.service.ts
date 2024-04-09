f204d1a416224b76856592502a07f6674624c3fe
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-04-09 20:00:25 +0200

    feat(drawer): open drawer with overlay

--- src/app/shared/services/drawer/drawer.service.ts
+++ src/app/shared/services/drawer/drawer.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
+import { OverlayOptions } from '@app/core/models/overlay/overlay-options.model';
 import { OverlayService } from '@app/core/services/overlay/overlay.service';
 import { DrawerComponent } from '@app/shared/components/drawer/drawer.component';
+import { takeUntil } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
@@ -9,6 +11,16 @@ export class DrawerService {
     constructor(private overlayService: OverlayService<DrawerComponent>) { }
 
     openDrawer(): void {
-        this.overlayService.show(DrawerComponent);
+        const options: OverlayOptions = {
+            directPosition: {},
+        };
+
+        const [componentRef, closeOverlay$] = this.overlayService.show(DrawerComponent, options);
+
+        componentRef.instance.closeDrawer
+            .pipe(takeUntil(closeOverlay$))
+            .subscribe(() => {
+                this.overlayService.close();
+            });
     }
 }
