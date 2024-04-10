0a9829a0ea36d0414656fcc07e7ab4ab7e2da219
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-04-10 19:25:23 +0200

    feat(drawer): add animation on start

--- src/app/shared/components/drawer/drawer.component.ts
+++ src/app/shared/components/drawer/drawer.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {
+    animate, style, transition, trigger,
+} from '@angular/animations';
+import { Component, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'bsa-drawer',
@@ -6,6 +9,22 @@ import { Component } from '@angular/core';
     imports: [],
     templateUrl: './drawer.component.html',
     styleUrl: './drawer.component.scss',
+    animations: [
+        trigger('drawerTrigger', [
+            transition(':enter', [
+                style({ transform: 'translateY(100%)' }),
+                animate('150ms', style({ transform: 'translateY(0)' })),
+            ]),
+            transition(':leave', [
+                animate('150ms', style({ transform: 'translateY(100%)' })),
+            ]),
+        ]),
+    ],
 })
 export class DrawerComponent {
+    closeDrawer = new EventEmitter<void>();
+
+    onCloseDrawer(): void {
+        this.closeDrawer.emit();
+    }
 }
