66ca772f192adbc08d352095110a581e2e008220
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-05-01 12:21:36 +0200

    refactor(navbar): switch language change dropdown to cdk overlay

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,74 +1,54 @@
+import { CdkConnectedOverlay, CdkOverlayOrigin } from '@angular/cdk/overlay';
 import { NgClass } from '@angular/common';
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Language } from '@app/core/enums/language/language.enum';
-import { EdgeX, EdgeY } from '@app/core/enums/overlay/relative-position-edge.enum';
 import { Theme } from '@app/core/enums/theme/theme.enum';
 import { LanguageService } from '@app/core/services/language/language.service';
-import { OverlayService } from '@app/core/services/overlay/overlay.service';
 import { ThemeService } from '@app/core/services/theme/theme.service';
 import { TranslateService } from '@app/core/services/translate/translate.service';
 import { DropdownContainerComponent } from '@app/shared/components/dropdown-container/dropdown-container.component';
-import { takeUntil } from 'rxjs';
+import { DropdownOption } from '@app/shared/models/dropdown-container/dropdown-option';
 
 @Component({
     selector: 'bsa-navbar',
     standalone: true,
-    imports: [NgClass, DropdownContainerComponent],
+    imports: [NgClass, DropdownContainerComponent, CdkConnectedOverlay, CdkOverlayOrigin],
     templateUrl: './navbar.component.html',
     styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
     @ViewChild('languageButton') languageButton: ElementRef<HTMLButtonElement> | null = null;
 
-    darkTheme: boolean = false;
+    protected darkTheme: boolean = false;
+    protected languageOptions = new Map<string, string>()
+        .set(Language.ENGLISH, this.translateService.translate('global.languageEN'))
+        .set(Language.POLISH, this.translateService.translate('global.languagePL'));
+    protected languageDropdownOpen = false;
 
     constructor(
         private languageService: LanguageService,
-        private overlayService: OverlayService<DropdownContainerComponent>,
         private themeService: ThemeService,
         private translateService: TranslateService,
+        private elementRef: ElementRef,
     ) {
         this.themeService.theme$.subscribe((theme) => {
             this.darkTheme = theme === Theme.DARK;
         });
     }
 
-    onSwitchTheme(): void {
+    protected onSwitchTheme(): void {
         this.themeService.switchTheme();
     }
 
-    onChangeLanguage(): void {
-        const languageOptions = new Map<string, string>()
-            .set(Language.ENGLISH, this.translateService.translate('global.languageEN'))
-            .set(Language.POLISH, this.translateService.translate('global.languagePL'));
+    protected onOutsideClick(event: MouseEvent): void {
+        if (this.elementRef.nativeElement.contains(event.target)) {
+            event.stopPropagation();
+        }
 
-        const overlayConfig = {
-            componentInputs: [
-                { name: 'options', value: languageOptions },
-            ],
-            backdrop: {
-                background: false,
-            },
-            relativePosition: {
-                relativeElement: this.languageButton!.nativeElement,
-                edgePositionX: {
-                    relativeEdge: EdgeX.RIGHT,
-                    contentEdge: EdgeX.RIGHT,
-                },
-                edgePositionY: {
-                    relativeEdge: EdgeY.BOTTOM,
-                    contentEdge: EdgeY.TOP,
-                },
-                width: 128,
-            },
-        };
+        this.languageDropdownOpen = false;
+    }
 
-        const overlay = this.overlayService.show(DropdownContainerComponent, overlayConfig);
-        overlay.componentRef.instance.selectOption
-            .pipe(takeUntil(overlay.close$))
-            .subscribe((value) => {
-                this.languageService.setLanguage(value.key as Language);
-                overlay.close();
-            });
+    protected onSelectOption(value: DropdownOption): void {
+        this.languageService.setLanguage(value.key as Language);
     }
 }
