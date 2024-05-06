109fc979f391db496037519ba54c221394e60714
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-05-06 19:00:03 +0200

    feat(dropdown-separator): add component

---
+++ src/app/shared/components/dropdown/dropdown-separator/dropdown-separator.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'bsa-dropdown-separator',
+    standalone: true,
+    imports: [],
+    template: '<div></div>',
+    styleUrl: './dropdown-separator.component.scss',
+})
+export class DropdownSeparatorComponent {}
