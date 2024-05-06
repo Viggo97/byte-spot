b9de7e6ebf02ff74cfa8238d783c578d9f99c30f
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-05-06 19:01:26 +0200

    feat(dropdown): add component

---
+++ src/app/shared/components/dropdown/dropdown.component.ts
@@ -0,0 +1,82 @@
+import { CdkTrapFocus } from '@angular/cdk/a11y';
+import {
+    Component, ContentChildren, ElementRef, HostListener, QueryList,
+} from '@angular/core';
+import { DropdownItemComponent } from '@app/shared/components/dropdown/dropdown-item/dropdown-item.component';
+import { Keycodes } from '@app/shared/enums/keycodes/keycodes.enum';
+
+@Component({
+    selector: 'bsa-dropdown',
+    standalone: true,
+    imports: [
+        CdkTrapFocus,
+    ],
+    templateUrl: './dropdown.component.html',
+    styleUrl: './dropdown.component.scss',
+})
+export class DropdownComponent {
+    @ContentChildren(DropdownItemComponent, { descendants: true, read: ElementRef })
+        items!: QueryList<ElementRef>;
+
+    private index = 0;
+
+    @HostListener('keydown', ['$event'])
+    onKeyboardAction(event: KeyboardEvent): void {
+        event.preventDefault();
+
+        const { code } = event;
+
+        switch (code) {
+            case Keycodes.ARROW_DOWN:
+                this.moveIndexAhead();
+                break;
+            case Keycodes.ARROW_UP:
+                this.moveIndexBack();
+                break;
+            case Keycodes.TAB:
+                this.onTab(event);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private moveIndexAhead(): void {
+        if (!this.isLastIndex) {
+            this.index += 1;
+            this.focusItem();
+        }
+    }
+
+    private moveIndexBack(): void {
+        if (!this.isFirstIndex) {
+            this.index -= 1;
+            this.focusItem();
+        }
+    }
+
+    private onTab(event: KeyboardEvent): void {
+        if (event.shiftKey) {
+            this.moveIndexBack();
+        } else {
+            this.moveIndexAhead();
+        }
+    }
+
+    private get isFirstIndex(): boolean {
+        return this.index === 0;
+    }
+
+    private get isLastIndex(): boolean {
+        return this.index === this.items.length - 1;
+    }
+
+    private focusItem(): void {
+        const item = this.items.get(this.index)?.nativeElement;
+        item?.focus({ preventScroll: true });
+        item?.scrollIntoView({
+            behavior: 'instant',
+            block: 'nearest',
+        });
+    }
+}
