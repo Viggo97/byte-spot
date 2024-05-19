abf6c0a29064f24febf0a61cf38538d5177dad0c
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-05-19 20:14:31 +0200

    feat(suggestions): add component

--- src/app/features/offers-overview/components/suggestions/suggestions.component.ts
+++ src/app/features/offers-overview/components/suggestions/suggestions.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { SuggestionsGroup } from '@app/features/offers-overview/model/suggestions-group.model';
+import { DropdownGroupComponent } from '@app/shared/components/dropdown/dropdown-group/dropdown-group.component';
+import { DropdownItem } from '@app/shared/components/dropdown/dropdown-item.model';
+import { DropdownItemComponent } from '@app/shared/components/dropdown/dropdown-item/dropdown-item.component';
+import {
+    DropdownSeparatorComponent,
+} from '@app/shared/components/dropdown/dropdown-separator/dropdown-separator.component';
 
 @Component({
-  selector: 'bsa-suggestions',
-  standalone: true,
-  imports: [],
-  templateUrl: './suggestions.component.html',
-  styleUrl: './suggestions.component.scss'
+    selector: 'bsa-suggestions',
+    standalone: true,
+    imports: [
+        DropdownGroupComponent,
+        DropdownItemComponent,
+        DropdownSeparatorComponent,
+    ],
+    templateUrl: './suggestions.component.html',
+    styleUrl: './suggestions.component.scss',
 })
 export class SuggestionsComponent {
+    @Input() suggestions: SuggestionsGroup[] = [];
 
+    onSelectItem(item: DropdownItem): void {
+    }
 }
