aee60cacb232d3bd78cd95ac38dbcfbf4d88b30f
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-05-21 18:59:48 +0200

    feat(offers-search-input): add component

--- src/app/features/offers-overview/components/input/input.component.ts
+++ src/app/features/offers-overview/components/input/input.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
-  selector: 'bsa-input',
-  standalone: true,
-  imports: [],
-  templateUrl: './input.component.html',
-  styleUrl: './input.component.scss'
+    selector: 'bsa-offers-search-input',
+    standalone: true,
+    imports: [],
+    templateUrl: './input.component.html',
+    styleUrl: './input.component.scss',
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            multi: true,
+            useExisting: InputComponent,
+        },
+    ],
 })
-export class InputComponent {
+export class InputComponent implements ControlValueAccessor {
+    private onChange!: (value: string) => void;
 
+    onChangeInput(phrase: string): void {
+        this.onChange(phrase);
+    }
+
+    registerOnChange(fn: (value: string) => void): void {
+        this.onChange = fn;
+    }
+
+    registerOnTouched(): void {
+    }
+
+    writeValue(): void {
+    }
 }
