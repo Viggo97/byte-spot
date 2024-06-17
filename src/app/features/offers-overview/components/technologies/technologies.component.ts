82e8c6240f40c531cb5cd7105118827770f143ce
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-06-17 06:48:42 +0200

    feat(offers-technologies): add component

---
+++ src/app/features/offers-overview/components/technologies/technologies.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'bsa-technologies',
+    standalone: true,
+    imports: [],
+    templateUrl: './technologies.component.html',
+    styleUrl: './technologies.component.scss',
+})
+export class TechnologiesComponent {
+
+}
