1b57e87c753c2689048158fe49790ea6f8e6a05f
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-06-18 21:08:39 +0200

    feat(pages-manager): add component

---
+++ src/app/shared/components/pages-manager/pages-manager.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bsa-pages-manager',
+  standalone: true,
+  imports: [],
+  templateUrl: './pages-manager.component.html',
+  styleUrl: './pages-manager.component.scss'
+})
+export class PagesManagerComponent {
+
+}
