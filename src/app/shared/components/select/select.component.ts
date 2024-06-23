eb06de691045f11b02789e5f6f21f3053d8ca1fc
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-06-23 17:22:24 +0200

    feat(select): adapt to generic dropdown item model

--- src/app/shared/components/select/select.component.ts
+++ src/app/shared/components/select/select.component.ts
@@ -1,33 +1,37 @@
 import { CdkConnectedOverlay, CdkOverlayOrigin } from '@angular/cdk/overlay';
 import {
-    Component, Input,
+    Component, EventEmitter, Input, Output,
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { DropdownComponent } from '@app/shared/components/dropdown/dropdown.component';
+import { DropdownItem } from '@app/shared/components/dropdown/dropdown-item.model';
+import { DropdownItemComponent } from '@app/shared/components/dropdown/dropdown-item/dropdown-item.component';
 import { DropdownContainerComponent } from '@app/shared/components/dropdown-container/dropdown-container.component';
-import { DropdownOption } from '@app/shared/models/dropdown-container/dropdown-option';
 
 @Component({
     selector: 'bsa-select',
     standalone: true,
     imports: [
-        FormsModule,
-        CdkOverlayOrigin,
-        DropdownContainerComponent,
         CdkConnectedOverlay,
+        CdkOverlayOrigin,
+        DropdownComponent,
+        DropdownContainerComponent,
+        DropdownItemComponent,
+        FormsModule,
     ],
     templateUrl: './select.component.html',
     styleUrl: './select.component.scss',
 })
-export class SelectComponent {
-    @Input({ required: true }) options!: Map<string, string>;
-
+export class SelectComponent<T> {
+    @Input({ required: true }) options!: DropdownItem<T>[];
     @Input() numberOfVisibleOptions?: number;
 
-    protected value: string | null = '';
+    @Output() selectOption = new EventEmitter<DropdownItem<T>>();
 
-    protected open = false;
+    value: unknown = null;
+    open = false;
 
-    protected onSelectOption(option: DropdownOption): void {
+    protected onSelectItem(option: DropdownItem<T>): void {
         this.open = false;
         this.value = option.value;
     }
