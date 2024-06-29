770c98c6aa4bb7a840317d411fec71695046dd3d
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-06-29 21:10:54 +0200

    feat(pagination): clean up class

--- src/app/shared/components/pagination/pagination.component.ts
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -15,7 +15,6 @@ import { SelectComponent } from '@app/shared/components/select/select.component'
 export class PaginationComponent implements OnInit {
     @Input({ required: true }) total!: number;
     @Input() perPage = 20;
-    @Input() options: number[] = [];
 
     @Input() set currentPage(value: number) {
         this.page = value;
@@ -25,23 +24,9 @@ export class PaginationComponent implements OnInit {
 
     page = 1;
     pages = 1;
-    optionsMap = new Map<string, string>();
 
     ngOnInit(): void {
         this.pages = this.total / this.perPage;
-        this.setOptions();
-    }
-
-    private setOptions(): void {
-        if (this.options.length > 0) {
-            this.options.forEach((o) => this.optionsMap.set(o.toString(), o.toString()));
-        } else {
-            this.optionsMap = new Map<string, string>()
-                .set('10', '10')
-                .set('20', '20')
-                .set('50', '50')
-                .set('100', '100');
-        }
     }
 
     onNextPage(): void {
