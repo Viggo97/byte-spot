890f84a86ad4a10ea414e5e8351a059dfa148aae
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-07-31 21:47:18 +0200

    feat(resize-observer): create directive

---
+++ src/app/shared/directvies/resize-observer.directive.ts
@@ -0,0 +1,26 @@
+import {
+    Directive, ElementRef, EventEmitter, OnDestroy, OnInit, Output,
+} from '@angular/core';
+
+@Directive({
+    selector: '[bsaResizeObserver]',
+    standalone: true,
+})
+export class ResizeObserverDirective implements OnInit, OnDestroy {
+    observer!: ResizeObserver;
+
+    @Output() elementResize = new EventEmitter<ResizeObserverEntry>();
+
+    constructor(private elementRef: ElementRef) { }
+
+    ngOnInit(): void {
+        this.observer = new ResizeObserver((entries) => {
+            this.elementResize.emit(entries[0]);
+        });
+        this.observer.observe(this.elementRef.nativeElement);
+    }
+
+    ngOnDestroy(): void {
+        this.observer.unobserve(this.elementRef.nativeElement);
+    }
+}
