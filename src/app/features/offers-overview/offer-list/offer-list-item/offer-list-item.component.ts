a12dcf22e690deec13b7700d8f630f27d6a5f6f5
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-08-13 19:50:49 +0200

    feat(offer-list-item): add location icon

--- src/app/features/offers-overview/offer-list/offer-list-item/offer-list-item.component.ts
+++ src/app/features/offers-overview/offer-list/offer-list-item/offer-list-item.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 
+import { IconComponent } from '@app/shared/components/icon/icon.component';
+
 @Component({
-  selector: 'bsa-offer-list-item',
-  standalone: true,
-  imports: [],
-  templateUrl: './offer-list-item.component.html',
-  styleUrl: './offer-list-item.component.scss'
+    selector: 'bsa-offer-list-item',
+    standalone: true,
+    imports: [
+        IconComponent,
+    ],
+    templateUrl: './offer-list-item.component.html',
+    styleUrl: './offer-list-item.component.scss',
 })
 export class OfferListItemComponent {
 
