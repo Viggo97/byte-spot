d0763f40f872ac15fe6706bd8f150c7de2d93db1
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-08-14 20:03:57 +0200

    feat(offers-overview): add pagination

--- src/app/features/offers-overview/offers-overview.component.ts
+++ src/app/features/offers-overview/offers-overview.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { PaginationComponent } from '@app/shared/components/pagination/pagination.component';
+
 import { OfferFiltersComponent } from './offer-filters/offer-filters.component';
 import { OfferSearchComponent } from './offer-search/offer-search.component';
 import { OfferListComponent } from './offer-list/offer-list.component';
@@ -11,10 +13,13 @@ import { OfferListComponent } from './offer-list/offer-list.component';
         OfferFiltersComponent,
         OfferSearchComponent,
         OfferListComponent,
+        PaginationComponent,
     ],
     templateUrl: './offers-overview.component.html',
     styleUrl: './offers-overview.component.scss',
 })
 export class OffersOverviewComponent {
-
+    onPageChange(page: number): void {
+        console.log(page);
+    }
 }
