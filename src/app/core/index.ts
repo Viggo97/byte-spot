3600620fa04d1f25fc62e95e6b101b2866e23e38
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-08-20 21:52:12 +0200

    feat(translate): add pipe

--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -1,1 +1,2 @@
 export * from './translate/translate.service';
+export * from './translate/translate.pipe';
---
+++ src/app/core/translate/translate.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+import { TranslateService } from './translate.service';
+
+@Pipe({
+    name: 'translate',
+    standalone: true,
+})
+export class TranslatePipe implements PipeTransform {
+    constructor(private translateService: TranslateService) {
+    }
+
+    transform(key: string): string {
+        console.log(key);
+        return this.translateService.translate(key);
+    }
+}
