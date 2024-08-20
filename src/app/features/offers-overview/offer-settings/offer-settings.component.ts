6b874c3e3def07aa0dc3514a10af894061629726
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-08-20 21:04:15 +0200

    feat(offer-settings): add component

---
+++ src/app/features/offers-overview/offer-settings/offer-settings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'bsa-offer-settings',
+    standalone: true,
+    imports: [],
+    templateUrl: './offer-settings.component.html',
+    styleUrl: './offer-settings.component.scss',
+})
+export class OfferSettingsComponent {
+
+}
