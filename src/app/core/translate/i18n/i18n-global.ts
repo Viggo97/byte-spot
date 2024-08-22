0217a9720d076f1c10b90feef030267b342a499d
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-08-22 21:57:39 +0200

    feat(translations): add offer translations

--- src/app/core/translate/i18n/i18n-global.ts
+++ src/app/core/translate/i18n/i18n-global.ts
@@ -1,6 +1,6 @@
 import { I18nLanguageCode } from './i18n-language-code.model';
 
-export const i18nGlobal: { [key: string] : I18nLanguageCode } = {
+export const i18nGlobal: { [key: string]: I18nLanguageCode } = {
     languageEN: {
         en: 'English',
         pl: 'Angielski',
@@ -12,5 +12,9 @@ export const i18nGlobal: { [key: string] : I18nLanguageCode } = {
     search: {
         en: 'Search',
         pl: 'Wyszukaj',
+    },
+    sortBy: {
+        en: 'Sort by',
+        pl: 'Sortuj według',
     },
 };
---
+++ src/app/core/translate/i18n/i18n-offer.ts
@@ -0,0 +1,20 @@
+import { I18nLanguageCode } from './i18n-language-code.model';
+
+export const i18nOffer: { [key: string] : I18nLanguageCode } = {
+    jobsOffersFound: {
+        en: 'job offers',
+        pl: 'ofert pracy',
+    },
+    newest: {
+        en: 'Newest',
+        pl: 'Najnowsze',
+    },
+    highestSalary: {
+        en: 'Highest salary',
+        pl: 'Najwyższe wynagrodzenie',
+    },
+    lowestSalary: {
+        en: 'Lowest salary',
+        pl: 'Najniższe wynagrodzenie',
+    },
+};
