d0532b5f032cb388abd302a30a1cbc6dcf911de0
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-09-01 20:47:57 +0200

    feat(slider-markup): add component

---
+++ src/app/shared/components/slider/slider-markup/slider-markup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'bsa-slider-markup',
+    standalone: true,
+    imports: [],
+    templateUrl: './slider-markup.component.html',
+    styleUrl: './slider-markup.component.scss',
+})
+export class SliderMarkupComponent {
+
+}
