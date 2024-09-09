9ff55d6c6ce0baa54744ec9a9a4fe09de7ae8eca
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-09-09 19:34:27 +0200

    feat(shared): update index

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -7,9 +7,12 @@ export * from './components/icon/icon.component';
 export * from './components/pagination/pagination.component';
 export * from './components/select/select.component';
 export * from './components/select-button/select-button.component';
+export * from './components/slider/slider.component';
 
 export * from './directvies/resize-observer.directive';
 
 export * from './enums/keycodes.enum';
 
 export * from './pipes/number-formatter.pipe';
+
+export * from './utils/number.util';
