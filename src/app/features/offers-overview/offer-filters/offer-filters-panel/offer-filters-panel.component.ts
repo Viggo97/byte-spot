16c2f8c1963786bdf49297fcf592c27e1d877b3c
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-09-18 20:56:56 +0200

    feat(offer-filters-panel): add component

---
+++ src/app/features/offers-overview/offer-filters/offer-filters-panel/offer-filters-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bsa-offer-filters-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './offer-filters-panel.component.html',
+  styleUrl: './offer-filters-panel.component.scss'
+})
+export class OfferFiltersPanelComponent {
+
+}
