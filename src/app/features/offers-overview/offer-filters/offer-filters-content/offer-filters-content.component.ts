9087eae0e162572b1aee6cb6dabc0f546aa34735
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-09-19 06:55:20 +0200

    feat(offer-filters-content): rename component and move template from filters

---
+++ src/app/features/offers-overview/offer-filters/offer-filters-content/offer-filters-content.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { CheckboxComponent, IconComponent, SliderComponent, ToggleComponent } from '@shared';
+import { TranslatePipe } from '@core';
+import { ControlValue } from '@app/features/offers-overview/types/control-value.type';
+
+@Component({
+    selector: 'bsa-offer-filters-content',
+    standalone: true,
+    imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        CheckboxComponent,
+        IconComponent,
+        SliderComponent,
+        ToggleComponent,
+        TranslatePipe,
+    ],
+    templateUrl: './offer-filters-content.component.html',
+    styleUrl: './offer-filters-content.component.scss',
+})
+export class OfferFiltersContentComponent {
+    @Input({ required: true }) form!: FormGroup;
+    @Input({ required: true }) technologies!: ControlValue[];
+    @Input({ required: true }) locations!: ControlValue[];
+}
