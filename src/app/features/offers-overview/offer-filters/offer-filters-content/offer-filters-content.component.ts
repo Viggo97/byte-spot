025aff038b5b4e9c7e4ef2ac58935a2852bb1364
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-09-20 06:42:10 +0200

    feat(offer-filters-content): remove logic previously moved to parent component

--- src/app/features/offers-overview/offer-filters/offer-filters-content/offer-filters-content.component.ts
+++ src/app/features/offers-overview/offer-filters/offer-filters-content/offer-filters-content.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 import { CheckboxComponent, IconComponent, SliderComponent, ToggleComponent } from '@shared';
 import { TranslatePipe } from '@core';
@@ -9,7 +9,6 @@ import { ControlValue } from '@app/features/offers-overview/types/control-value.
     selector: 'bsa-offer-filters-content',
     standalone: true,
     imports: [
-        FormsModule,
         ReactiveFormsModule,
         CheckboxComponent,
         IconComponent,
@@ -24,5 +23,4 @@ export class OfferFiltersContentComponent {
     @Input({ required: true }) form!: FormGroup;
     @Input({ required: true }) technologies!: ControlValue[];
     @Input({ required: true }) locations!: ControlValue[];
-    @Input() displayHeader = false;
 }
