e63c4a47a8359f5e3cefc73adb71f9228d6a2fed
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2024-10-17 19:51:12 +0200

    feat(app): set Hash Location Strategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { languageInterceptor } from '@core';
@@ -8,7 +8,7 @@ import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
     providers: [
-        provideRouter(routes),
+        provideRouter(routes, withHashLocation()),
         provideAnimations(),
         provideHttpClient(withInterceptors([languageInterceptor])),
     ],
