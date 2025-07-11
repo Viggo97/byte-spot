4518c4a9c4e80532fbf062108a74fbd4bde1820a
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2025-07-11 20:17:06 +0200

    refactor(lib): clean up

--- projects/byte-spot/src/app/app.component.ts
+++ projects/byte-spot/src/app/app.component.ts
@@ -3,12 +3,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { NavbarComponent } from '@core';
-import { ByteSpotLibComponent } from "@byte-spot-lib";
 
 @Component({
     selector: 'bsa-root',
     standalone: true,
-    imports: [CommonModule, RouterOutlet, NavbarComponent, ByteSpotLibComponent],
+    imports: [CommonModule, RouterOutlet, NavbarComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
