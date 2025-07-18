75b088107c23391d028770f4cbc756b8349838d2
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2025-07-18 18:17:46 +0200

    chore(eslint): restore config and update

---
+++ eslint.config.js
@@ -0,0 +1,96 @@
+import { defineConfig } from 'eslint/config';
+import * as ts from 'typescript-eslint';
+import * as angular from 'angular-eslint';
+import eslintPluginImport from 'eslint-plugin-import';
+import eslintUnusedImports from 'eslint-plugin-unused-imports';
+import eslintStylistic from '@stylistic/eslint-plugin';
+
+export default defineConfig(
+    {
+        basePath: 'projects',
+        files: ['**/*.ts'],
+        languageOptions: {
+            parserOptions: {
+                project: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        extends: [
+            ts.configs.recommended,
+            ...ts.configs.stylistic,
+            ...ts.configs.strictTypeChecked,
+            ...angular.configs.tsRecommended,
+            eslintPluginImport.flatConfigs.recommended,
+            eslintPluginImport.flatConfigs.typescript,
+        ],
+        plugins: {
+            'unused-imports': eslintUnusedImports,
+            '@stylistic': eslintStylistic,
+        },
+        rules: {
+            'semi': 'error',
+            'prefer-const': 'error',
+            'import/prefer-default-export': 'off',
+            'import/no-unresolved': 'off',
+            'import/first': 'error',
+            'import/newline-after-import': 'error',
+            'no-duplicate-imports': 'error',
+            'unused-imports/no-unused-imports': 'error',
+            'unused-imports/no-unused-vars': [
+                'warn',
+                {
+                    'vars': 'all',
+                    'varsIgnorePattern': '^_',
+                    'args': 'after-used',
+                    'argsIgnorePattern': '^_',
+                },
+            ],
+            'max-len': [
+                'error',
+                120,
+                {
+                    'ignorePattern': '^import\\s.+\\sfrom\\s.+;$',
+                },
+            ],
+            'class-methods-use-this': 'off',
+            'no-underscore-dangle': 'off',
+            'object-curly-newline': [
+                'error',
+                {
+                    'ObjectExpression': { 'multiline': true },
+                    'ObjectPattern': { 'multiline': true },
+                    'ImportDeclaration': 'never',
+                    'ExportDeclaration': 'never',
+                },
+            ],
+            '@stylistic/indent': ['error', 4],
+            '@stylistic/quotes': ['error', 'single'],
+            'comma-dangle': ['error', 'always-multiline'],
+            '@typescript-eslint/no-explicit-any': 'error',
+            '@angular-eslint/directive-selector': [
+                'error',
+                {
+                    'type': 'attribute',
+                    'prefix': 'bsa',
+                    'style': 'camelCase',
+                },
+            ],
+            '@angular-eslint/component-selector': [
+                'error',
+                {
+                    'type': 'element',
+                    'prefix': 'bsa',
+                    'style': 'kebab-case',
+                },
+            ],
+        },
+    },
+    {
+        basePath: 'projects',
+        files: ['**/*.html'],
+        extends: [
+            ...angular.configs.templateRecommended,
+            ...angular.configs.templateAccessibility,
+        ],
+    },
+);
