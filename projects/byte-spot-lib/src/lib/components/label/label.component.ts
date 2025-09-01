999fa00ca35cef004937ba10b7bcbd4613e28e82
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2025-09-01 20:22:28 +0200

    refactor(label): simplify implementation

--- projects/byte-spot-lib/src/lib/components/label/label.component.ts
+++ projects/byte-spot-lib/src/lib/components/label/label.component.ts
@@ -3,10 +3,8 @@ import { Component, input } from '@angular/core';
 @Component({
     selector: 'ngx-bsa-label',
     imports: [],
-    template: `
-      <span [id]="id"><ng-content></ng-content></span>
-    `,
-    styleUrl: './label.component.scss',
+    template: '<ng-content></ng-content>',
+    host: {'[attr.id]': 'id()'},
 })
 export class LabelComponent {
     id = input.required<string>();
