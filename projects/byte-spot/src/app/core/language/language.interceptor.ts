6ccbf7c8274d1be4685c16272a4e11aabfa02de3
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2025-11-23 12:38:13 +0100

    fix(language): fix import

--- projects/byte-spot/src/app/core/language/language.interceptor.ts
+++ projects/byte-spot/src/app/core/language/language.interceptor.ts
@@ -1,7 +1,7 @@
+import { inject } from '@angular/core';
 import { HttpEvent, HttpHandlerFn, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { inject } from '@angular/core';
-import { LanguageService } from '@core';
+import { LanguageService } from './language.service';
 
 export function languageInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
     const lang = inject(LanguageService).language;
