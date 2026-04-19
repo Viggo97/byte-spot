b61033ffe67e8a46c55c1bf07c331a4cb23a6bb3
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2026-04-19 15:13:42 +0200

    feat(i18n): Add user translations

--- projects/byte-spot/src/app/core/translate/i18n/i18n-user.ts
+++ projects/byte-spot/src/app/core/translate/i18n/i18n-user.ts
@@ -17,8 +17,76 @@ export const i18nUser: Record<string, I18nLanguageCode> = {
         en: 'Sign In',
         pl: 'Zaloguj się',
     },
-    Logout: {
+    logout: {
         en: 'Logout',
         pl: 'Wyloguj',
+    },
+    email: {
+        en: 'E-mail',
+        pl: 'E-mail',
+    },
+    password: {
+        en: 'Password',
+        pl: 'Hasło',
+    },
+    firstName: {
+        en: 'First name',
+        pl: 'Imię',
+    },
+    lastName: {
+        en: 'Last name',
+        pl: 'Nazwisko',
+    },
+    emailRequired: {
+        en: 'E-mail is required.',
+        pl: 'E-mail jest wymagany.',
+    },
+    emailInvalid: {
+        en: 'E-mail is invalid.',
+        pl: 'Adres e-mail jest niepoprawany.',
+    },
+    emailMinLength: {
+        en: 'E-mail must have at least 4 characters.',
+        pl: 'E-mail musi mieć conajmniej 4 znaki.',
+    },
+    emailMaxLength: {
+        en: 'E-mail must not have more than 64 characters.',
+        pl: 'E-mail nie może mieć więcej niż 64 znaki.',
+    },
+    passwordRequired: {
+        en: 'Password is required.',
+        pl: 'Hasło jest wymagane.',
+    },
+    passwordMinLength: {
+        en: 'Password must contain at least 8 characters.',
+        pl: 'Hasło musi zawierać conajmniej 8 znaków.',
+    },
+    passwordMaxLength: {
+        en: 'Password must not have more than 100 characters.',
+        pl: 'Hasło nie może mieć więcej niż 100 znaków.',
+    },
+    firstNameRequired: {
+        en: 'First name is required.',
+        pl: 'Imię jest wymagane.',
+    },
+    firstNameMinLength: {
+        en: 'First name must have at least 2 characters.',
+        pl: 'Imię musi mieć conajmniej 2 znaki.',
+    },
+    firstNameMaxLength: {
+        en: 'First name must not have more than 32 characters.',
+        pl: 'Imię nie może mieć więcej niż 32 znaki.',
+    },
+    lastNameRequired: {
+        en: 'Last name is required.',
+        pl: 'Nazwisko jest wymagane.',
+    },
+    lastNameMinLength: {
+        en: 'Last name must have at least 3 characters.',
+        pl: 'Nazwisko musi mieć conajmniej 3 znaki.',
+    },
+    lastNameMaxLength: {
+        en: 'Last name must not have more than 64 characters.',
+        pl: 'Nazwisko nie może mieć więcej niż 64 znaki.',
     },
 };
