d7191ec3bc908774aa696166b40913d9d0bfeeac
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2026-04-19 15:12:09 +0200

    refactor(navbar): Handle different screen width

--- projects/byte-spot/src/app/core/layout/navbar/navbar.component.ts
+++ projects/byte-spot/src/app/core/layout/navbar/navbar.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { BreakpointObserver } from '@angular/cdk/layout';
 import { NavbarThemeSwitchComponent } from './theme-switch/navbar-theme-switch.component';
 import { NavbarLanguageSwitchComponent } from './language-switch/navbar-language-switch.component';
+import { SignInSelectComponent } from './sign-in-select/sign-in-select.component';
+import CompactMenuDrawerComponent from './compact-menu-drawer/compact-menu-drawer.component';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
     selector: 'bsa-navbar',
-    imports: [NavbarThemeSwitchComponent, NavbarLanguageSwitchComponent],
+    imports: [
+        NgOptimizedImage,
+        RouterLink,
+        CompactMenuDrawerComponent,
+        NavbarThemeSwitchComponent,
+        NavbarLanguageSwitchComponent,
+        SignInSelectComponent,
+    ],
     templateUrl: './navbar.component.html',
     styleUrl: './navbar.component.scss',
 })
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class
-export class NavbarComponent {}
+export class NavbarComponent {
+    private readonly _breakpointObserver = inject(BreakpointObserver);
+    private readonly _destroyRef = inject(DestroyRef);
+    private readonly _authService = inject(AuthService);
+
+    compactMode = signal(window.innerWidth < 960);
+    isLoggedIn = toSignal(this._authService.isLoggedIn$);
+
+    constructor() {
+        this.subscribeToBreakpointObserver();
+    }
+
+    private subscribeToBreakpointObserver(): void {
+        this._breakpointObserver.observe('(min-width: 960px)')
+            .pipe(takeUntilDestroyed(this._destroyRef))
+            .subscribe((state) => {
+                this.compactMode.set(!state.matches);
+            });
+    }
+}
