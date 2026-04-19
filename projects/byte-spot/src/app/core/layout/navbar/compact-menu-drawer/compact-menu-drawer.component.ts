61bc80ba433246c6a72f36e6c4220fd43be3aabf
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2026-04-19 15:08:00 +0200

    feat(auth): Add compact menu drawer

---
+++ projects/byte-spot/src/app/core/layout/navbar/compact-menu-drawer/compact-menu-drawer.component.ts
@@ -0,0 +1,50 @@
+import { Component, inject, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { CdkConnectedOverlay, CdkOverlayOrigin, Overlay } from '@angular/cdk/overlay';
+import { DrawerComponent } from 'ngx-bsl';
+import { SvgIconComponent } from '@shared';
+import { TranslatePipe } from '../../../translate/translate.pipe';
+import { NavbarLanguageSwitchComponent } from '../language-switch/navbar-language-switch.component';
+import { NavbarThemeSwitchComponent } from '../theme-switch/navbar-theme-switch.component';
+import { AuthService } from '../../../auth/auth.service';
+
+@Component({
+    selector: 'bsa-compact-menu-drawer',
+    imports: [
+        CdkOverlayOrigin,
+        CdkConnectedOverlay,
+        TranslatePipe,
+        DrawerComponent,
+        SvgIconComponent,
+        NavbarLanguageSwitchComponent,
+        NavbarThemeSwitchComponent,
+    ],
+    templateUrl: './compact-menu-drawer.component.html',
+    styleUrl: './compact-menu-drawer.component.scss',
+})
+class CompactMenuDrawerComponent {
+    private readonly _router = inject(Router);
+    private readonly _overlay = inject(Overlay);
+    private readonly _authService = inject(AuthService);
+
+    protected readonly scrollStrategy = this._overlay.scrollStrategies.block();
+
+    isLoggedIn = toSignal(this._authService.isLoggedIn$);
+    protected drawerOpen = signal(false);
+
+    protected openDrawer(): void {
+        this.drawerOpen.set(true);
+    }
+
+    protected closeDrawer(): void {
+        this.drawerOpen.set(false);
+    }
+
+    protected navigate(route: string): void {
+        this.closeDrawer();
+        void this._router.navigate([route]);
+    }
+}
+
+export default CompactMenuDrawerComponent;
