58d9fd01e30e34d3dd566c470cd0d8a9888be85f
Bartosz Stawiarski <stawiarski.bartosz@gmail.com>
2026-05-26 12:46:03 +0200

    refactor(lib): Migrate drawer

---
+++ projects/byte-spot-lib/src/lib/components/drawer/drawer.component.ts
@@ -0,0 +1,53 @@
+import {afterNextRender,
+    ChangeDetectionStrategy,
+    Component,
+    ElementRef,
+    inject,
+    input,
+    output,
+    signal,
+    ViewEncapsulation} from '@angular/core';
+import {CdkTrapFocus} from '@angular/cdk/a11y';
+
+@Component({
+    selector: 'bsl-drawer',
+    imports: [
+        CdkTrapFocus,
+    ],
+    templateUrl: './drawer.component.html',
+    styleUrl: './drawer.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    encapsulation: ViewEncapsulation.None,
+    host: {
+        'role': 'dialog',
+        'aria-modal': 'true',
+        '[attr.aria-label]': 'title()',
+        '[class.bsl-drawer-closed]': 'isClosed()',
+        '(transitionend)': 'onTransitionEnd($event)',
+    },
+})
+export class DrawerComponent {
+    title = input.required<string>();
+    closed = output();
+    protected isClosed = signal(true);
+    private elementRef = inject(ElementRef);
+
+    constructor() {
+        afterNextRender(() => {
+            this.isClosed.set(false);
+        });
+    }
+
+    close(): void {
+        this.isClosed.set(true);
+        this.closed.emit();
+    }
+
+    protected onTransitionEnd(event: TransitionEvent) {
+        if (this.elementRef.nativeElement === event.target) {
+            if (this.isClosed()) {
+                this.closed.emit();
+            }
+        }
+    }
+}
